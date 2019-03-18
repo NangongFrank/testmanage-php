ddbb2e79ea4fd5c93cc6edfe16b378bfcbeb756f
zhao windows <8413@qq.com>
2019-03-18 10:55:56 +0800

    create php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @Author: name
+ * @Date:   2019-03-18 10:54:36
+ * @Last Modified by:   name
+ * @Last Modified time: 2019-03-18 10:55:25
+ */
+header("Access-Control-Allow-Origin:*");
