771f9eb67a2d50a7cafab75127deab3c9857e110
zhao windows <8413@qq.com>
2019-03-18 13:00:56 +0800

    mvc

---
+++ base/DBModel.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @Author: name
+ * @Date:   2019-03-18 11:18:13
+ * @Last Modified by:   name
+ * @Last Modified time: 2019-03-18 11:18:45
+ */
+require '../test.config.php';
+
+class DBModel {
+
+}
---
+++ test.config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Author: name
+ * @Date:   2019-03-18 11:15:34
+ * @Last Modified by:   name
+ * @Last Modified time: 2019-03-18 11:18:54
+ */
+$DBINFO = array(
+    "host": "localhost",
+    "port": "3306",
+    "dbName": "testmg",
+    "user": "root",
+    "pwd": "",
+    "charset": "utf-8"
+);
+
